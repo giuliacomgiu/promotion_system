3be423f41a47d1c5d96c17d3876922e73e0ecb43
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-06 17:24:41 -0300

    Criar uma promocao

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,4 +6,20 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id])
   end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    redirect_to @promotion if @promotion.save
+  end
+
+  def promotion_params
+    params
+      .require(:promotion)
+      .permit(:name, :description, :code, :discount_rate,
+              :expiration_date, :coupon_quantity)
+  end
 end
