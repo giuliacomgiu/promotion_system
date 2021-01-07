a5ce9e963d2da1b8496254674f69fade101c79bf
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-07 15:06:38 -0300

    Removendo testes unitarios redundantes.

--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -29,40 +29,33 @@ describe Promotion do
     end
   end
 
-  context 'deletion' do
-    it 'is destroyed successfully' do
-      promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                                    code: 'NATAL10', discount_rate: 10,
-                                    coupon_quantity: 100, expiration_date: '22/12/2033')
-      promotion.destroy!
-
-      expect(promotion.destroyed?).to be true
-    end
-  end
-
   context 'editing' do
-    it 'edits successfully' do
-      promotion = Promotion.new(name: 'Natal', description: 'Promoção de Natal',
-                                    code: 'NATAL10', discount_rate: 10,
-                                    coupon_quantity: 100, expiration_date: '22/12/2033')
-      promotion.update!(name: 'Pascoa', code: 'PASCOA20', description: 'Promoção de páscoa')
-
-      expect(promotion.name).to eq 'Pascoa'
-      expect(promotion.code).to eq 'PASCOA20'
-      expect(promotion.description).to eq 'Promoção de páscoa'
-      expect(promotion.discount_rate).to eq 10
-    end
-
-    it 'validates successfully and doesnt update' do
+    it 'validates code\'s uniqueness' do
       Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
                         code: 'NATAL10', discount_rate: 10,
                         coupon_quantity: 100, expiration_date: '22/12/2033')
-      promotion = Promotion.new(name: '', code: 'NATAL10')
+      promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                                    code: 'NATAL20', discount_rate: 10,
+                                    coupon_quantity: 100, expiration_date: '22/12/2033')
 
-      promotion.valid?
+      promotion.update(code: 'NATAL10')
+
+      expect(promotion.errors[:code]).to include('deve ser único')
+    end
+
+    it 'validates non blank fields' do
+      promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                                    code: 'NATAL20', discount_rate: 10,
+                                    coupon_quantity: 100, expiration_date: '22/12/2033')
+
+      promotion.update(name: '', code: '', discount_rate: '',
+                        coupon_quantity: '', expiration_date: '')
 
       expect(promotion.errors[:name]).to include('não pode ficar em branco')
-      expect(promotion.errors[:code]).to include('deve ser único')
+      expect(promotion.errors[:code]).to include('não pode ficar em branco')
+      expect(promotion.errors[:discount_rate]).to include('não pode ficar em branco')
+      expect(promotion.errors[:coupon_quantity]).to include('não pode ficar em branco')
+      expect(promotion.errors[:expiration_date]).to include('não pode ficar em branco')
     end
   end
 end
