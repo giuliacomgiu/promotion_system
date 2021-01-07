33bfc7d21158ef6d00162a13ef1c2a55db867d55
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-07 16:14:43 -0300

    WIP mais testes para categorias de produtos

--- spec/features/admin_view_product_category.rb
+++ spec/features/admin_view_product_category.rb
@@ -12,5 +12,12 @@ feature 'Admin views product category' do
     expect(page).to have_content('HOSP')
     expect(page).to have_content('E-mail')
     expect(page).to have_content('EMAIL')
+    expect(page).to have_link('Voltar', href: root_path)
+  end
+
+  xscenario 'and shows empty message' do
+  end
+
+  xscenario 'and views details' do
   end
 end
