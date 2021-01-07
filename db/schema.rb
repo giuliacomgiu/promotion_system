bd6e67618f6d1013fef3cbe486c346e91d041c34
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-07 16:11:17 -0300

    WIP ver categorias de produto

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,23 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_174336) do
+ActiveRecord::Schema.define(version: 2021_01_07_185205) do
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["code"], name: "index_product_categories_on_code", unique: true
+  end
 
   create_table "promotions", force: :cascade do |t|
     t.string "name"
