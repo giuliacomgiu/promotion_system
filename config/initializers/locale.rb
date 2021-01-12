9052423442f2ee2f10c186e38737760a83232bc2
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-12 13:59:54 -0300

    WIP i18n models

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,5 @@
 # config/initializers/locale.rb
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # Permitted locales available for the application
 I18n.available_locales = %i[en pt-BR]
