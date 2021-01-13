b4f61ff75501c3706d5a813bb6b92913c157cdd8
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-13 15:13:28 -0300

    Fixing typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  resources :promotions
+  resources :promotions do
+    post 'issue_coupons', on: :member
+  end
   resources :product_categories
 end
