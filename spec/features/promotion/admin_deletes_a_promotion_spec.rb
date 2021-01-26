a49d962f47c9f17a969e741a1d22d0e07a7a653f
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-01-26 10:38:49 -0300

    Using FactoryBot

--- spec/features/promotion/admin_deletes_a_promotion_spec.rb
+++ spec/features/promotion/admin_deletes_a_promotion_spec.rb
@@ -1,34 +1,27 @@
 require 'rails_helper'
 
 feature 'Admin deletes a promotion' do
-  background do
-    user = User.create!(email: 'test@locaweb.com.br', password: 'f4k3p455w0rd')
-    login_as(user, scope: :user)
-  end
+  let!(:creator){ create :user, email: 'maria@locaweb.com.br' }
 
   scenario 'and it succeeds' do
-    product = ProductCategory.create!(name: 'Wordpress', code: 'WORDP')
-    Promotion.create!(product_categories: [product], name: 'Cyber Monday', coupon_quantity: 90,
-                      description: 'Promoção de Cyber Monday', code: 'CYBER15', discount_rate: 15,
-                      expiration_date: '22/12/2033', maximum_discount:10)
+    login_as(creator, scope: :user)
+    create :promotion, :with_product_category, creator: creator
 
     visit root_path
     click_on 'Promoções'
-    click_on 'Cyber Monday'
+    click_on 'Natal'
     click_on 'Deletar'
 
     expect(page).to have_content('Nenhuma promoção cadastrada')
   end
 
   scenario 'and it fails, so not found page is rendered' do
-    product = ProductCategory.create!(name: 'Wordpress', code: 'WORDP')
-    promotion = Promotion.create!(product_categories: [product], name: 'Cyber Monday', coupon_quantity: 5, 
-                                  description: 'Promoção de Cyber Monday', code: 'CYBER15', 
-                                  discount_rate: 15, expiration_date: '22/12/2033', maximum_discount:10)
+    login_as(creator, scope: :user)
+    promotion = create :promotion, :with_product_category, creator: creator
 
     visit root_path
     click_on 'Promoções'
-    click_on 'Cyber Monday'
+    click_on 'Natal'
     promotion.destroy!
     click_on 'Deletar'
 
