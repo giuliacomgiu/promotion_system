db0840aecc4f8771d41c7291324383238246805f
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2021-02-16 16:45:14 -0300

    feat creating database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+creator = User.create!(email: 'creator@locaweb.com.br', password: '123456')
+
+curator = User.create!(email: 'curator@locaweb.com.br', password: '123456')
+
+product_categories = ProductCategory.create!([{name: 'Wordpress', code: 'WORDP'},
+                                              {name: 'Email', code: 'EMAIL'}])
+
+# Pending promotion
+Promotion.create!(name: 'Natal Dez', description: 'O espírito natalino na Locaweb!',
+                  code: 'NATAL10', discount_rate: 5, coupon_quantity: 10,
+                  maximum_discount: 20, expiration_date: 1.day.from_now,
+                  product_category_ids: product_categories.map(&:id), creator: creator)
+
+# Expired pending promotion
+Promotion.create!(name: 'Pascoa Dez', description: 'O espírito natalino na Locaweb!',
+                  code: 'PASCOA10', discount_rate: 5, coupon_quantity: 10,
+                  maximum_discount: 20, expiration_date: 1.year.ago,
+                  product_category_ids: product_categories.map(&:id), creator: creator)
+
+# Approved promotion
+Promotion.create!(name: 'Carnaval Dez', description: 'O espírito natalino na Locaweb!',
+                  code: 'CARNA10', discount_rate: 5, coupon_quantity: 10,
+                  maximum_discount: 20, expiration_date: 1.day.from_now, curator: curator,
+                  product_category_ids: product_categories.map(&:id), creator: creator)
+
+# Approved promotion with coupons
+promo = Promotion.create!(name: 'Locaweb Dez', description: 'O espírito natalino na Locaweb!',
+                          code: 'LOCAW10', discount_rate: 5, coupon_quantity: 3,
+                          maximum_discount: 20, expiration_date: 1.day.from_now, curator: curator,
+                          product_category_ids: product_categories.map(&:id), creator: creator)
+promo.coupons.create!([{code: 'LOCAW10-0001'}, 
+                       {code: 'LOCAW10-0002', status: :archived},
+                       {code: 'LOCAW10-0003', status: :burned, order: '12345'}])
